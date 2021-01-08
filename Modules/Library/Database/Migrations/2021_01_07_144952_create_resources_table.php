651d7e375026af2ddf38c459a4918c5966e26751
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-01-08 19:00:25 +0500

    author nullable

--- Modules/Library/Database/Migrations/2021_01_07_144952_create_resources_table.php
+++ Modules/Library/Database/Migrations/2021_01_07_144952_create_resources_table.php
@@ -16,7 +16,7 @@ class CreateResourcesTable extends Migration
         Schema::create('resources', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('title', 256);
-            $table->string('author', 128);
+            $table->string('author', 128)->nullable();
             $table->string('year', 4)->nullable();
             $table->text('description')->nullable();
             $table->boolean('is_public')->default(false);
