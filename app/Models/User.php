75962a51771de6e142a4e4e3b5cdeda0f58344ee
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-01-10 18:01:55 +0500

    Epic #13 - User Profile
    delete user -> soft delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
     use UsesUUID;
+    use SoftDeletes;
 
     public $incrementing = false;
     protected $primaryKey = 'id';
