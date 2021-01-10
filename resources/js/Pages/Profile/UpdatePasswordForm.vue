2050e4ac48c872174ed01a21a7b0e6c47d5a0f01
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-01-10 17:20:43 +0500

    Epic #13 - User Profile
    Change password

--- resources/js/Pages/Profile/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/UpdatePasswordForm.vue
@@ -1,83 +1,75 @@
 <template>
-    <jet-form-section @submitted="updatePassword">
-        <template #title>
-            Update Password
-        </template>
+    <form @submit.prevent="updatePassword">
+        <b-card title="Изменение пароля"
+                sub-title="Придумайте надежный длинный пароль с использованием специальных символов">
+            <b-card-body>
+                <b-col md="6">
+                    <b-form-group label="Текущий пароль" :state="!form.error('current_password')" ref="current_password"
+                                  :invalid-feedback="form.error('current_password')" autocomplete="current-password">
+                        <b-input type="password" v-model="form.current_password"></b-input>
+                    </b-form-group>
+                    <b-form-group label="Новый пароль" :state="!form.error('password')" ref="password"
+                                  :invalid-feedback="form.error('password')" autocomplete="new-password">
+                        <b-input type="password" v-model="form.password"></b-input>
+                    </b-form-group>
+                    <b-form-group label="Подтверждение пароля" :state="!form.error('password_confirmation')"
+                                  ref="password_confirmation"
+                                  :invalid-feedback="form.error('password_confirmation')" autocomplete="new-password">
+                        <b-input type="password" v-model="form.password_confirmation"></b-input>
+                    </b-form-group>
+                </b-col>
+            </b-card-body>
 
-        <template #description>
-            Ensure your account is using a long, random password to stay secure.
-        </template>
-
-        <template #form>
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="current_password" value="Current Password" />
-                <jet-input id="current_password" type="password" class="mt-1 block w-full" v-model="form.current_password" ref="current_password" autocomplete="current-password" />
-                <jet-input-error :message="form.error('current_password')" class="mt-2" />
-            </div>
-
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="password" value="New Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" autocomplete="new-password" />
-                <jet-input-error :message="form.error('password')" class="mt-2" />
-            </div>
-
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" autocomplete="new-password" />
-                <jet-input-error :message="form.error('password_confirmation')" class="mt-2" />
-            </div>
-        </template>
-
-        <template #actions>
-            <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                Saved.
-            </jet-action-message>
-
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </jet-button>
-        </template>
-    </jet-form-section>
+            <template #footer>
+                <b-row align-v="baseline">
+                    <b-card-text class="ml-4" v-if="form.recentlySuccessful">Пароль изменён</b-card-text>
+                    <b-button type="submit" class="ml-auto mr-4" variant="info" :disabled="form.processing">
+                        Сохранить
+                    </b-button>
+                </b-row>
+            </template>
+        </b-card>
+    </form>
 </template>
 
 <script>
-    import JetActionMessage from '@/Jetstream/ActionMessage'
-    import JetButton from '@/Jetstream/Button'
-    import JetFormSection from '@/Jetstream/FormSection'
-    import JetInput from '@/Jetstream/Input'
-    import JetInputError from '@/Jetstream/InputError'
-    import JetLabel from '@/Jetstream/Label'
+import JetActionMessage from '@/Jetstream/ActionMessage'
+import JetButton from '@/Jetstream/Button'
+import JetFormSection from '@/Jetstream/FormSection'
+import JetInput from '@/Jetstream/Input'
+import JetInputError from '@/Jetstream/InputError'
+import JetLabel from '@/Jetstream/Label'
 
-    export default {
-        components: {
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-        },
+export default {
+    components: {
+        JetActionMessage,
+        JetButton,
+        JetFormSection,
+        JetInput,
+        JetInputError,
+        JetLabel,
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    current_password: '',
-                    password: '',
-                    password_confirmation: '',
-                }, {
-                    bag: 'updatePassword',
-                }),
-            }
-        },
+    data() {
+        return {
+            form: this.$inertia.form({
+                current_password: '',
+                password: '',
+                password_confirmation: '',
+            }, {
+                bag: 'updatePassword',
+            }),
+        }
+    },
 
-        methods: {
-            updatePassword() {
-                this.form.put(route('user-password.update'), {
-                    preserveScroll: true
-                }).then(() => {
-                    this.$refs.current_password.focus()
-                })
-            },
+    methods: {
+        updatePassword() {
+            this.form.put(route('user-password.update'), {
+                preserveScroll: true
+            }).then(() => {
+                this.$refs.current_password.focus()
+            })
         },
-    }
+    },
+}
 </script>
