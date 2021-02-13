8b6350c9dd286e02f3531c8026ef7627d639f42c
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-02-13 10:23:39 +0500

    children image

--- resources/js/Pages/Resource/Index.vue
+++ resources/js/Pages/Resource/Index.vue
@@ -1,123 +1,137 @@
 <template>
-    <div>
-        <b-jumbotron>
-            <b-container fluid>
-                <b-row>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-input size="sm" placeholder="Ном" v-model="filters.title"></b-form-input>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-input size="sm" placeholder="Муаллиф" v-model="filters.author"></b-form-input>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-input size="sm" placeholder="Сол" v-model="filters.year"></b-form-input>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.type">
-                            <b-form-select-option :value="null">-- Намуд --</b-form-select-option>
-                            <b-form-select-option value="video">Видео</b-form-select-option>
-                            <b-form-select-option value="audio">Аудио</b-form-select-option>
-                            <b-form-select-option value="ebook">Электронная книга</b-form-select-option>
-                            <b-form-select-option value="pdf">PDF</b-form-select-option>
-                            <b-form-select-option value="other">Другое</b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.language">
-                            <b-form-select-option :value="null">-- Забон --</b-form-select-option>
-                            <b-form-select-option value="ru">Русӣ</b-form-select-option>
-                            <b-form-select-option value="tj">Тоҷикӣ</b-form-select-option>
-                            <b-form-select-option value="en">Англисӣ</b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.level">
-                            <b-form-select-option :value="null">-- Выберите уровень --</b-form-select-option>
-                            <b-form-select-option value="easy">Легко</b-form-select-option>
-                            <b-form-select-option value="hard">Сложно</b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.category">
-                            <b-form-select-option :value="null">-- Фан --</b-form-select-option>
-                            <b-form-select-option v-for="category in categories" :value="category.id"
-                                                  :key="category.id">{{ category.name }}
-                            </b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.theme">
-                            <b-form-select-option :value="null">-- Мавзӯъ --</b-form-select-option>
-                            <b-form-select-option v-for="theme in themes" :value="theme.id"
-                                                  :key="theme.id">{{ theme.name }}
-                            </b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.grade">
-                            <b-form-select-option :value="null">-- Синф --</b-form-select-option>
-                            <b-form-select-option v-for="grade in grades" :value="grade.id"
-                                                  :key="grade.id">{{ grade.name }}
-                            </b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <b-form-select size="sm" v-model="filters.role">
-                            <b-form-select-option :value="null">-- Истифодабар --</b-form-select-option>
-                            <b-form-select-option v-for="role in roles" :value="role.id"
-                                                  :key="role.id">{{ role.name }}
-                            </b-form-select-option>
-                        </b-form-select>
-                    </b-form-group>
-                    <b-form-group class="col-sm-12 col-md-3">
-                        <suggestion-input displayPropertyName="name"
-                                          ref="tagInput"
-                                          placeholder="Теги"
-                                          size="sm"
-                                          @selected="onTagSelected"
-                                          action-url="/tags/filtered/"
-                                          show
-                        >
-                        </suggestion-input>
-                    </b-form-group>
-                </b-row>
-                <b-row>
-                    <b-form-group class="col-12">
-                        <ul
-                            class="list-unstyled d-inline-flex flex-wrap mb-0">
-                            <b-card
-                                v-for="tag in filters.tags"
-                                :key="tag.name"
-                                :id="`tags_${tag.name.replace(/\s/g, '_')}_`"
-                                tag="li"
-                                class="mt-1 mr-1 bg-light"
-                                body-class="py-1 pr-2 text-nowrap"
-                            >
-                                <strong>{{ tag.name }}</strong>
-                                <b-button
-                                    @click="removeTag(tag.name)"
-                                    variant="link"
-                                    class="text-decoration-none"
-                                    size="sm"
-                                    :aria-controls="`albumsTagList__${tag.name.replace(/\s/g, '_')}_`"
-                                >&times;
-                                </b-button>
-                            </b-card>
-                        </ul>
+    <b-container fluid>
+        <div class="position-relative">
+            <div class="overlay">
+                <b-img class="w-100 " src="/images/main-page-banner.jpg"></b-img>
+            </div>
+        </div>
 
-                    </b-form-group>
-                </b-row>
-                <b-row align-h="center">
-                    <b-button size="sm" variant="info" @click="filter">Нигох доштан</b-button>
-                </b-row>
-            </b-container>
+        <b-row>
+            <div class="col-12">
+                <b-jumbotron>
+                    <b-container fluid>
+                        <b-row>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-input size="sm" placeholder="Ном" v-model="filters.title"></b-form-input>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-input size="sm" placeholder="Муаллиф" v-model="filters.author"></b-form-input>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-input size="sm" placeholder="Сол" v-model="filters.year"></b-form-input>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.type">
+                                    <b-form-select-option :value="null">-- Намуд --</b-form-select-option>
+                                    <b-form-select-option value="video">Видео</b-form-select-option>
+                                    <b-form-select-option value="audio">Аудио</b-form-select-option>
+                                    <b-form-select-option value="ebook">Электронная книга</b-form-select-option>
+                                    <b-form-select-option value="pdf">PDF</b-form-select-option>
+                                    <b-form-select-option value="other">Другое</b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.language">
+                                    <b-form-select-option :value="null">-- Забон --</b-form-select-option>
+                                    <b-form-select-option value="ru">Русӣ</b-form-select-option>
+                                    <b-form-select-option value="tj">Тоҷикӣ</b-form-select-option>
+                                    <b-form-select-option value="en">Англисӣ</b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.level">
+                                    <b-form-select-option :value="null">-- Выберите уровень --</b-form-select-option>
+                                    <b-form-select-option value="easy">Легко</b-form-select-option>
+                                    <b-form-select-option value="hard">Сложно</b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.category">
+                                    <b-form-select-option :value="null">-- Фан --</b-form-select-option>
+                                    <b-form-select-option v-for="category in categories" :value="category.id"
+                                                          :key="category.id">{{ category.name }}
+                                    </b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.theme">
+                                    <b-form-select-option :value="null">-- Мавзӯъ --</b-form-select-option>
+                                    <b-form-select-option v-for="theme in themes" :value="theme.id"
+                                                          :key="theme.id">{{ theme.name }}
+                                    </b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.grade">
+                                    <b-form-select-option :value="null">-- Синф --</b-form-select-option>
+                                    <b-form-select-option v-for="grade in grades" :value="grade.id"
+                                                          :key="grade.id">{{ grade.name }}
+                                    </b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <b-form-select size="sm" v-model="filters.role">
+                                    <b-form-select-option :value="null">-- Истифодабар --</b-form-select-option>
+                                    <b-form-select-option v-for="role in roles" :value="role.id"
+                                                          :key="role.id">{{ role.name }}
+                                    </b-form-select-option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group class="col-sm-12 col-md-3">
+                                <suggestion-input displayPropertyName="name"
+                                                  ref="tagInput"
+                                                  placeholder="Теги"
+                                                  size="sm"
+                                                  @selected="onTagSelected"
+                                                  action-url="/tags/filtered/"
+                                                  show
+                                >
+                                </suggestion-input>
+                            </b-form-group>
+                        </b-row>
+                        <b-row>
+                            <b-form-group class="col-12">
+                                <ul
+                                    class="list-unstyled d-inline-flex flex-wrap mb-0">
+                                    <b-card
+                                        v-for="tag in filters.tags"
+                                        :key="tag.name"
+                                        :id="`tags_${tag.name.replace(/\s/g, '_')}_`"
+                                        tag="li"
+                                        class="mt-1 mr-1 bg-light"
+                                        body-class="py-1 pr-2 text-nowrap"
+                                    >
+                                        <strong>{{ tag.name }}</strong>
+                                        <b-button
+                                            @click="removeTag(tag.name)"
+                                            variant="link"
+                                            class="text-decoration-none"
+                                            size="sm"
+                                            :aria-controls="`albumsTagList__${tag.name.replace(/\s/g, '_')}_`"
+                                        >&times;
+                                        </b-button>
+                                    </b-card>
+                                </ul>
 
-        </b-jumbotron>
-        <resource-masonry-with-data-fetching ref="resources-list"
-                                             :resources="this.pagination.data"
-                                             :url="fetchUrl">
-        </resource-masonry-with-data-fetching>
-    </div>
+                            </b-form-group>
+                        </b-row>
+                        <b-row align-h="center">
+                            <b-button size="sm" variant="info" @click="filter">Нигох доштан</b-button>
+                        </b-row>
+                    </b-container>
+
+                </b-jumbotron>
+            </div>
+
+        </b-row>
+        <b-row>
+            <resource-masonry-with-data-fetching ref="resources-list"
+                                                 :resources="this.pagination.data"
+                                                 :url="fetchUrl">
+            </resource-masonry-with-data-fetching>
+        </b-row>
+
+    </b-container>
 </template>
 
 <script>
@@ -185,7 +199,7 @@ export default {
                 this.fetchUrl += `&grade=${this.filters.grade}`;
             if (this.filters.role)
                 this.fetchUrl += `&role=${this.filters.role}`;
-            if(this.filters.tags)
+            if (this.filters.tags)
                 this.fetchUrl += `&tags=` + this.filters.tags.map(t => t.id).join();
 
 
@@ -203,3 +217,40 @@ export default {
     }
 }
 </script>
+
+<style>
+.overlay::before {
+    content: "";
+    position: absolute;
+    background-color: #00000066;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.overlay::after {
+    content: "“Хонед, Омӯзед ва Рушд ёбед” – тавассути ин китобхонаи электронӣ.";
+    color: white;
+    position: absolute;
+    text-align: center;
+    top: 20%;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    font-size: 1rem;
+}
+
+@media only screen and (min-width: 400px) {
+    .overlay::after {
+        top: 30% !important;
+    }
+}
+
+@media only screen and (min-width: 768px) {
+    .overlay::after {
+        font-size: 1.8rem !important;
+        top: 45% !important;
+    }
+}
+</style>
