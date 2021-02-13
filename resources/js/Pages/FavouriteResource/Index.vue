81fa99ab09f73dc02112426786253d93a9d2c92a
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-02-13 17:01:01 +0500

    translation

--- resources/js/Pages/FavouriteResource/Index.vue
+++ resources/js/Pages/FavouriteResource/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <b-col cols="12">
-        <h1 class="mb-4">Избранное</h1>
+        <h1 class="mb-4">Интихобшуда</h1>
         <resource-masonry-with-data-fetching :resources="pagination.data"
                                              :url="route('my-resources')">
         </resource-masonry-with-data-fetching>
