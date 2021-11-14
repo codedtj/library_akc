4dcc8b120287b3a20d0eb335b78607e7164b0442
SN <43911665+smmdzzv@users.noreply.github.com>
2021-11-14 11:58:29 +0500

    translate categories

--- resources/js/Pages/CategoryResource/Index.vue
+++ resources/js/Pages/CategoryResource/Index.vue
@@ -2,8 +2,8 @@
     <b-col md="8" class="mx-auto simple-list">
         <ul>
             <li v-for="category in categories">
-                <a class="list-item" :href="route('category-resources.show',category.id)"
-                   :key="category.id">{{ category.name }}</a>
+                <a class="list-item text-capitalize" :href="route('category-resources.show',category.id)"
+                   :key="category.id"> {{$t('label.' + category.name.replaceAll(' ', '_')) }}</a>
             </li>
         </ul>
     </b-col>
