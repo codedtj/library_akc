c378bed1bdee427b7e9aeda1646d2c253d69f70a
SN <43911665+smmdzzv@users.noreply.github.com>
2021-11-15 11:56:04 +0500

    translation

--- resources/js/Pages/FavouriteResource/Index.vue
+++ resources/js/Pages/FavouriteResource/Index.vue
@@ -1,6 +1,6 @@
 <template>
-    <b-col cols="12">
-        <h1 class="mb-4">Интихобшуда</h1>
+    <b-col class="mt-5" cols="12">
+        <h1 class="mb-4 first-letter-capitalize">{{ $t('label.favourite') }}</h1>
         <resource-masonry-with-data-fetching :resources="pagination.data"
                                              :url="route('my-resources')">
         </resource-masonry-with-data-fetching>
--- resources/js/Pages/MyResource/Index.vue
+++ resources/js/Pages/MyResource/Index.vue
@@ -1,6 +1,6 @@
 <template>
-    <b-col cols="12">
-        <h1 class="mb-4">Маводҳои ман</h1>
+    <b-col class="mt-5" cols="12">
+        <h1 class="mb-4 first-letter-capitalize">{{ $t('label.my_resources') }}</h1>
         <resource-masonry-with-data-fetching :resources="pagination.data"
                                              :url="route('my-resources')">
         </resource-masonry-with-data-fetching>
