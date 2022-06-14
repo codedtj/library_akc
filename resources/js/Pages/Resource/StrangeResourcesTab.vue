daf95c486eaa6edb5640bc1933c959c0e0c1571e
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2022-06-14 22:26:09 +0500

    strange resources links

--- resources/js/Pages/Resource/StrangeResourcesTab.vue
+++ resources/js/Pages/Resource/StrangeResourcesTab.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container-fluid">
-        <div v-for="resource in resources" class="d-flex">
-            <a :href="resource.url" class="text-decoration-none" target="_blank">
+        <div  class="d-flex flex-wrap">
+            <a v-for="resource in resources" :href="resource.url" class="text-decoration-none d-block pr-3 pb-3" target="_blank">
                 <div class="bg-info rounded p-5 ">
-                    <span class="text-white" style="font-size: 2rem">{{resource.title}}</span>
+                    <span class="text-white" style="font-size: 1.1rem">{{resource.title}}</span>
                 </div>
             </a>
         </div>
@@ -17,8 +17,24 @@ export default {
         return{
             resources:[
                 {
-                    url:'/resources/lughatomuzi/story.html',
+                    url:'/resources/1/story.html',
                     title:'Луғатомӯзӣ'
+                },
+                {
+                    url:'/resources/2/story.html',
+                    title:'Усули аз амалия ба назария'
+                },
+                {
+                    url:'/resources/3/story.html',
+                    title:'Тарзи рушд додани салоҳияти тафаккури интиқодии хонандагон'
+                },
+                {
+                    url:'/resources/4/story.html',
+                    title:'ШДХСМ'
+                },
+                {
+                    url:'/resources/5/story.html',
+                    title:'Санъат ва меҳнат'
                 }
             ]
         }
