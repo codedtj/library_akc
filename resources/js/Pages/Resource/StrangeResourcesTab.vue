793550e5f66bfb5de841f8a9843af63da7099113
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2022-06-14 21:20:14 +0500

    strange resources added to index

---
+++ resources/js/Pages/Resource/StrangeResourcesTab.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-fluid">
+        <div v-for="resource in resources" class="d-flex">
+            <a :href="resource.url" class="text-decoration-none" target="_blank">
+                <div class="bg-info rounded p-5 ">
+                    <span class="text-white" style="font-size: 2rem">{{resource.title}}</span>
+                </div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StrangeResourcesTab",
+    data(){
+        return{
+            resources:[
+                {
+                    url:'/resources/lughatomuzi/story.html',
+                    title:'Луғатомӯзӣ'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
